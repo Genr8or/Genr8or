d4c5de6a9abb3dc0f331af32b55ed89c7fe8f337
T Savo <evilgenius@nefariousplan.com>
2018-09-22 01:29:10 -0700

    Make the unit tests buy, then sell, and assert the balances work out.

--- test/TestGenr8or.sol
+++ test/TestGenr8or.sol
@@ -5,6 +5,9 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Genr8or.sol";
 
 contract TestGenr8or {
+
+    uint public initialBalance = 1 ether;
+
     Genr8or genr8or = Genr8or(DeployedAddresses.Genr8or());
 
     function testGenr8or() public {
@@ -13,7 +16,18 @@ contract TestGenr8or {
         assert(gen.counterBalance() == 0);
         gen.buy.value(1 ether)();
         assert(gen.counterBalance() == 1 ether);
+        assert(gen.totalTokens() == 1 ether);
+        assert(gen.myTokens() == 1 ether);
+        gen.sell(1 ether);
+        assert(gen.myTokens() == 0 ether);
+        assert(gen.totalTokens() == 0 ether);
+        assert(gen.counterBalance() == 0.1 ether);
         
+
     }
+
+    function () public payable {
+    // This will NOT be executed when Ether is sent. \o/
+  }
 
 }
